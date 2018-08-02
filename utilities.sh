cc67b41c3b4b20222e48b23b4eb4508b47e342ce
Kieran Bamforth <hello@kieranbamforth.me>
2018-08-02 11:19:56 +0100

    Moving FZF config to .zshrc Plugin section.

--- utilities.sh
+++ utilities.sh
@@ -80,42 +80,6 @@ function dkpwn {
     docker stop $1 && docker rm $1
 }
 # }}}
-# {{{ FZF.
-_gen_fzf_default_opts() {
-    local base03="234"
-    local base02="235"
-    local base01="240"
-    local base00="241"
-    local base0="244"
-    local base1="245"
-    local base2="254"
-    local base3="230"
-    local yellow="136"
-    local orange="166"
-    local red="160"
-    local magenta="125"
-    local violet="61"
-    local blue="33"
-    local cyan="37"
-    local green="64"
-
-    # Comment and uncomment below for the light theme.
-
-    # Solarized Dark color scheme for fzf
-    export FZF_DEFAULT_OPTS="
-    --color fg:-1,bg:-1,hl:$blue,fg+:$base2,bg+:$base02,hl+:$blue
-    --color info:$yellow,prompt:$yellow,pointer:$base3,marker:$base3,spinner:$yellow
-    "
-}
-_gen_fzf_default_opts
-function fzf_git_checkout() {
-    local branches branch
-	branches=$(git branch -vv) &&
-	branch=$(echo "$branches" | fzf +m) &&
-	git checkout $(echo "$branch" | awk '{print $1}' | sed "s/.* //")
-}
-
-# }}}
 # AWS. {{{
 function aws_asg_scaleup {
     ASG=$1
