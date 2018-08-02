7777439fd72465683078659834ee40cf678503c8
Kieran Bamforth <hello@kieranbamforth.me>
2018-08-02 23:23:35 +0100

    Adding osx.sh from private dots.

---
+++ osx.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+sudo defaults delete /Library/Preferences/com.apple.loginwindow LoginwindowText
+defaults write com.apple.dock checked-for-launchpad -bool false
+defaults write com.apple.dock magnification -bool false
+defaults write com.apple.dock orientation -string "right"
+defaults write com.apple.dock persistent-apps -array "()"
+defaults write com.apple.dock tilesize -int 48
+
+defaults write com.apple.screencapture disable-shadow -bool true
+
+# Dont show accent selector.
+defaults write NSGlobalDomain AppleInterfaceStyle -string "Dark"
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+# Set function keys as default.
+defaults write NSGlobalDomain com.apple.keyboard.fnState -bool true
+# Expand save / print panels by default
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+
+/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:showItemInfo true" ~/Library/Preferences/com.apple.finder.plist
+
+chflags nohidden ~/Library
+
+sudo killall Dock Finder SystemUIServer
