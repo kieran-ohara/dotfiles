4f56d49d5e93941a8374f747b3c04cf98bd8fed0
Kieran Bamforth <hello@kieranbamforth.me>
2021-08-08 16:24:31 +0100

    Update keyboard.

--- keyboards/ergodox_ez/default/config.h
+++
@@ -1,9 +0,0 @@
-/*
-  Set any config.h overrides for your specific keymap here.
-  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
-*/
-#define ORYX_CONFIGURATOR
-#define DISABLE_RGB_MATRIX_DIGITAL_RAIN
-#define DISABLE_RGB_MATRIX_ALPHAS_MODS
-#define DISABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
-#define DISABLE_RGB_MATRIX_RAINDROPS
--- keyboards/ergodox_ez/default/rules.mk
+++
@@ -1,8 +0,0 @@
-# Set any rules.mk overrides for your specific keymap here.
-# See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
-LINK_TIME_OPTIMIZATION_ENABLE = yes
-COMMAND_ENABLE = no
-MOUSEKEY_ENABLE = no
-SRC = matrix.c
-RGBLIGHT_ENABLE = no
-RGB_MATRIX_ENABLE = yes
