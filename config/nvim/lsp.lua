ee2faa83dde4f8650142b16de782291ff13cc0f2
Kieran Bamforth <hello@kieranbamforth.me>
2022-05-02 00:52:52 +0100

    Adding a comma.

--- config/nvim/lsp.lua
+++ config/nvim/lsp.lua
@@ -28,7 +28,7 @@ require'lspconfig'.efm.setup{
       'sh',
       'typescript',
       'typescriptreact',
-      'yaml'
+      'yaml',
     },
     -- sent in initializationOptions from nvim to lang server.
     -- see https://github.com/mattn/efm-langserver#configuration
