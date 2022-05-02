c03b1f6ef13ed6e69b40d6a55fae75f9c8cc5eca
Kieran Bamforth <hello@kieranbamforth.me>
2022-05-02 02:42:09 +0100

    Add autocomplete for language server.

--- config/nvim/initlua.lua
+++ config/nvim/initlua.lua
@@ -9,6 +9,7 @@ cmp.setup({
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
     },
     sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
       { name = 'buffer' },
     })
 })
@@ -17,6 +18,10 @@ cmp.setup.cmdline(':', {
     { name = 'path' }
   })
 })
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+require('lspconfig')['lua'].setup {
+  capabilities = capabilities
+}
 
 require'nvim-treesitter.configs'.setup {
     ensure_installed = {},
