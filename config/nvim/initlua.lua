2042381cb01ec620df1079151ad8e21b2d080267
Kieran Bamforth <hello@kieranbamforth.me>
2022-05-02 01:25:17 +0100

    Add shortcuts for adding hunks again.

--- config/nvim/initlua.lua
+++ config/nvim/initlua.lua
@@ -75,4 +75,30 @@ require'nvim-treesitter.configs'.setup {
     },
 }
 
-require('gitsigns').setup()
+require('gitsigns').setup({
+  numhl = true,
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    map('n', ']c', function()
+      if vim.wo.diff then return ']c' end
+      vim.schedule(function() gs.next_hunk() end)
+      return '<Ignore>'
+    end, {expr=true})
+
+    map('n', '[c', function()
+      if vim.wo.diff then return '[c' end
+      vim.schedule(function() gs.prev_hunk() end)
+      return '<Ignore>'
+    end, {expr=true})
+
+    map('n', '<leader>hs', gs.stage_buffer)
+    map('n', '<leader>hd', gs.toggle_deleted)
+  end
+})
