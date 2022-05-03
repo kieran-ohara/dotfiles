2c8b362422cfac8e623548595c58652f4385a894
Kieran Bamforth <hello@kieranbamforth.me>
2022-05-03 23:35:47 +0100

    Refactor the parsers.

--- config/nvim/parser-src/build-parsers.sh
+++ config/nvim/parser-src/build-parsers.sh
@@ -6,14 +6,14 @@ then
 fi
 
 LOCKFILE="$XDG_CONFIG_HOME/nvim/pack/treesitter/start/nvim-treesitter/lockfile.json"
-if [ ! -f $LOCKFILE ]; then
+if [ ! -f "$LOCKFILE" ]; then
     echo "Lockfile not found (searched $LOCKFILE)"
     exit 1
 fi
 
 function get_revision() {
     LANGUAGE=$1;
-    jq -r ".$LANGUAGE.revision" < $LOCKFILE
+    jq -r ".$LANGUAGE.revision" < "$LOCKFILE"
 }
 
 for LANG in \
@@ -31,37 +31,17 @@ do
     make build/$LANG.so LANG=$LANG REVISION="$(get_revision $LANG)"
 done
 
-make build/dockerfile.so \
-    LANG=dockerfile \
-    REPO=camdencheek/tree-sitter-dockerfile.git \
-    REVISION="$(get_revision dockerfile)"
-
-make build/yaml.so \
-    LANG=yaml \
-    REPO=ikatyang/tree-sitter-yaml.git \
-    REVISION="$(get_revision yaml)"
-
-make build/hcl.so \
-    LANG=hcl \
-    REPO=MichaHoffmann/tree-sitter-hcl.git \
-    REVISION="$(get_revision hcl)"
-
-make build/lua.so \
-    LANG=lua \
-    REPO=MunifTanjim/tree-sitter-lua.git \
-    REVISION="$(get_revision lua)"
-
-make build/markdown.so \
-    LANG=markdown \
-    REPO=MDeiml/tree-sitter-markdown.git \
-    REVISION="$(get_revision markdown)"
-
 declare extras
 extras=(
+  dockerfile camdencheek/tree-sitter-dockerfile
   dot rydesun/tree-sitter-dot
+  hcl MichaHoffmann/tree-sitter-hcl
+  lua MunifTanjim/tree-sitter-lua
   make alemuller/tree-sitter-make
+  markdown MDeiml/tree-sitter-markdown
   vim vigoux/tree-sitter-viml
   vue ikatyang/tree-sitter-vue
+  yaml ikatyang/tree-sitter-yaml
 )
 
 # Requires bash 5...
