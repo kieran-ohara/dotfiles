540d135f5930d600f9b78f7b6deef46292ef0ff0
Kieran Bamforth <hello@kieranbamforth.me>
2022-05-06 17:46:42 +0100

    Apply the honourary format.

--- config/nvim/lsp.lua
+++ config/nvim/lsp.lua
@@ -1,35 +1,37 @@
-require 'lspkind'.init {}
-require 'lspsaga'.init_lsp_saga()
+require "lspkind".init {}
+require "lspsaga".init_lsp_saga()
 
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-  -- Enable underline, use default values
-  underline = true,
-  -- Enable virtual text, override spacing to 4
-  virtual_text = {
-    spacing = 4,
-  },
-}
+vim.lsp.handlers["textDocument/publishDiagnostics"] =
+  vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics,
+  {
+    -- Enable underline, use default values
+    underline = true,
+    -- Enable virtual text, override spacing to 4
+    virtual_text = {
+      spacing = 4
+    }
+  }
 )
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
-require 'lspconfig'.tsserver.setup {
+require "lspconfig".tsserver.setup {
   capabilities = capabilities
 }
 
-local lspconfig = require 'lspconfig'
-local util = require 'lspconfig.util'
-local configs = require 'lspconfig.configs'
+local lspconfig = require "lspconfig"
+local util = require "lspconfig.util"
+local configs = require "lspconfig.configs"
 
 configs.efmWithFormat = {
   default_config = {
-    cmd = { 'efm-langserver' },
+    cmd = {"efm-langserver"},
     root_dir = util.find_git_ancestor,
     single_file_support = true,
     filetypes = {
-      'lua',
-      'sh',
+      "lua",
+      "sh"
     },
     init_options = {
       documentFormatting = true,
@@ -37,24 +39,24 @@ configs.efmWithFormat = {
       documentSymbol = true,
       codeAction = true,
       completion = true
-    },
-  },
+    }
+  }
 }
-lspconfig.efmWithFormat.setup{}
+lspconfig.efmWithFormat.setup {}
 
 configs.efmNoFormat = {
   default_config = {
-    cmd = { 'efm-langserver' },
+    cmd = {"efm-langserver"},
     root_dir = util.find_git_ancestor,
     single_file_support = true,
     filetypes = {
-      'dockerfile',
-      'javascript',
-      'javascriptreact',
-      'markdown',
-      'typescript',
-      'typescriptreact',
-      'yaml',
+      "dockerfile",
+      "javascript",
+      "javascriptreact",
+      "markdown",
+      "typescript",
+      "typescriptreact",
+      "yaml"
     },
     init_options = {
       documentFormatting = false,
@@ -62,52 +64,51 @@ configs.efmNoFormat = {
       documentSymbol = true,
       codeAction = true,
       completion = true
-    },
-  },
+    }
+  }
 }
-lspconfig.efmNoFormat.setup{}
+lspconfig.efmNoFormat.setup {}
 
-
-local runtime_path = vim.split(package.path, ';')
+local runtime_path = vim.split(package.path, ";")
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
-require 'lspconfig'.sumneko_lua.setup {
+require "lspconfig".sumneko_lua.setup {
   settings = {
     Lua = {
       runtime = {
         -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
+        version = "LuaJIT",
         -- Setup your lua path
-        path = runtime_path,
+        path = runtime_path
       },
       diagnostics = {
         -- Get the language server to recognize the `vim` global
-        globals = { 'vim' },
+        globals = {"vim"}
       },
       workspace = {
         -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
+        library = vim.api.nvim_get_runtime_file("", true)
       },
       -- Do not send telemetry data containing a randomized but unique identifier
       telemetry = {
-        enable = false,
-      },
-    },
-  },
+        enable = false
+      }
+    }
+  }
 }
 
-require('lspconfig').yamlls.setup {
+require("lspconfig").yamlls.setup {
   filetypes = {
-    'json',
-    'yaml'
+    "json",
+    "yaml"
   },
   settings = {
     yaml = {
       schemas = {
         ["https://json.schemastore.org/package.json"] = "package.json",
-        ["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.20.9-standalone/all.json"] = "/infrastructure/kubernetes/**/*.yml",
-      },
-    },
+        ["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.20.9-standalone/all.json"] = "/infrastructure/kubernetes/**/*.yml"
+      }
+    }
   }
 }
