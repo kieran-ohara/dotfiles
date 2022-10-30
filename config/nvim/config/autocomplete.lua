3474f33d17c04970e3b03ae09dd62d032fd1670c
Kieran Bamforth <hello@kieranbamforth.me>
2022-10-30 22:08:06 +0000

    Add signature help.

--- config/nvim/config/autocomplete.lua
+++ config/nvim/config/autocomplete.lua
@@ -14,6 +14,7 @@ cmp.setup(
     }),
     sources = cmp.config.sources(
       {
+        { name = 'nvim_lsp_signature_help' },
         { name = "nvim_lsp" },
         { name = "vsnip" },
         { name = "buffer" }
