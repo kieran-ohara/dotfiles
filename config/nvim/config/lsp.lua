f031fa159a2629ae55832f78faca539bf4a6b78b
Kieran Bamforth <hello@kieranbamforth.me>
2022-10-30 21:50:04 +0000

    Use icons for the diagnostic signs.

--- config/nvim/config/lsp.lua
+++ config/nvim/config/lsp.lua
@@ -2,6 +2,12 @@
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
 -- Nice icons
 require "lspkind".init {}
 
