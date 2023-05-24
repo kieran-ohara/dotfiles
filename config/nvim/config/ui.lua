bcb19f59eed6b356eba3399016ab27b56fc4a616
Kieran O'Hara <hello@kieranohara.me>
2023-05-24 12:26:30 +0000

    Add Telescope shortcuts for commits and git status.

--- config/nvim/config/ui.lua
+++ config/nvim/config/ui.lua
@@ -30,7 +30,10 @@ require'nvim-web-devicons'.setup({})
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fF', builtin.git_status, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fc', builtin.git_commits, {})
+vim.keymap.set('n', '<leader>fC', builtin.git_bcommits, {})
 vim.keymap.set('n', '<leader>fu', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.command_history, {})
 
