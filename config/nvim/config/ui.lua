236b22c8b89dcc23bbc08f5388ce282087c43730
Kieran O'Hara <hello@kieranohara.me>
2023-08-14 11:02:14 +0100

    Set shortcut for chat.

--- config/nvim/config/ui.lua
+++ config/nvim/config/ui.lua
@@ -48,13 +48,13 @@ require("which-key").setup({
       suggestions = 20, -- how many suggestions should be shown in the list?
     },
     presets = {
-      operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
+      operators = true, -- adds help for operators like d, y, ... and registers them for motion / text object completion
       motions = false, -- adds help for motions
-      text_objects = false, -- help for text objects triggered after entering an operator
+      text_objects = true, -- help for text objects triggered after entering an operator
       windows = true, -- default bindings on <c-w>
       nav = false, -- misc bindings to work with windows
       z = false, -- bindings for folds, spelling and others prefixed with z
-      g = false, -- bindings for prefixed with g
+      g = true, -- bindings for prefixed with g
     },
   },
   window = {
@@ -100,3 +100,4 @@ require('lualine').setup({
 })
 
 require("chatgpt").setup()
+vim.keymap.set('n', '<leader>c', '<cmd>ChatGPT<CR>', {  desc = 'ChatGPT' })
