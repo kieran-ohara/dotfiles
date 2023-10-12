f6416c373220a93a9d7680290f96c3631fa25ae9
Kieran O'Hara <hello@kieranohara.me>
2023-10-12 18:07:25 +0100

    Move user-facing brew tools to pkgx.

--- config/zsh/tools.zsh
+++ config/zsh/tools.zsh
@@ -1,3 +1,25 @@
-alias ansible='() { pkgx +ansible ansible "$@" }'
-alias ansible-playbook='() { pkgx +ansible ansible-playbook "$@" }'
+tools=(
+  "ansible-playbook"
+  "ansible"
+  "cw"
+  "gh"
+  "git-branchless"
+  "git"
+  "glab"
+  "hurl"
+  "jq"
+  "kubectl"
+  "lefthook"
+  "make"
+  "ncdu"
+  "packer"
+  "rg"
+  "tree"
+  "zoxide"
+)
+for tool in "${tools[@]}"
+do
+  alias $tool="() { pkgx +$tool $tool \"\$@\" }"
+done
+
 alias http='() { pkgx +httpie http "$@" }'
