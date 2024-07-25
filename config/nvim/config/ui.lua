86194b8bee617b002ef2a361a8612d5dc92aa941
Kieran O'Hara <hello@kieranohara.me>
2024-07-25 20:20:31 +0100

    Update vim plugins.

--- config/nvim/config/ui.lua
+++ config/nvim/config/ui.lua
@@ -57,22 +57,13 @@ require("which-key").setup({
       g = true, -- bindings for prefixed with g
     },
   },
-  window = {
-    border = "single", -- none, single, double, shadow
-    position = "bottom", -- bottom, top
-    margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
-    padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
-  },
   layout = {
     height = { min = 4, max = 25 }, -- min and max height of the columns
     width = { min = 20, max = 50 }, -- min and max width of the columns
     spacing = 3, -- spacing between columns
     align = "center", -- align columns left, center or right
   },
-  ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
-  hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
   show_help = true, -- show help message on the command line when the popup is visible
-  triggers = "auto", -- automatically setup triggers
 })
 
 require('lualine').setup({
