a30f1f2b596421151599129eb228706cf4ac4b51
Kieran O'Hara <hello@kieranohara.me>
2024-07-28 13:17:52 +0100

    Move git signs config to own config plugin.

---
+++ config/nvim/pack/vcs/start/config/plugin/config.lua
@@ -0,0 +1,27 @@
+require('gitsigns').setup({
+  numhl = true,
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    map('n', ']c', function()
+      if vim.wo.diff then return ']c' end
+      vim.schedule(function() gs.next_hunk() end)
+      return '<Ignore>'
+    end, { expr = true })
+
+    map('n', '[c', function()
+      if vim.wo.diff then return '[c' end
+      vim.schedule(function() gs.prev_hunk() end)
+      return '<Ignore>'
+    end, { expr = true })
+
+    map('n', '<leader>hs', gs.stage_buffer)
+    map('n', '<leader>hd', gs.toggle_deleted)
+  end
+})
