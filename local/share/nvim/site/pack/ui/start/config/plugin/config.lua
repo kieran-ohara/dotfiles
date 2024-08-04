10e7b082c48fbf44b78fcc849920dbdd61f4b165
Kieran O'Hara <hello@kieranohara.me>
2024-08-04 12:33:37 +0100

    Create packages for navigating vim/nvim.

--- local/share/nvim/site/pack/ui/start/config/plugin/config.lua
+++ local/share/nvim/site/pack/ui/start/config/plugin/config.lua
@@ -1,44 +1,5 @@
 require'nvim-web-devicons'.setup({})
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>fa', builtin.find_files, { desc = 'Find all files' })
-vim.keymap.set('n', '<leader>ff', builtin.git_files, { desc = 'Find git files' })
-vim.keymap.set('n', '<leader>fF', builtin.git_status, { desc = 'Git Status' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live Grep' })
-vim.keymap.set('n', '<leader>fc', builtin.git_commits, { desc = 'Git Commits' })
-vim.keymap.set('n', '<leader>fC', builtin.git_bcommits, { desc = 'Git BCommits' })
-vim.keymap.set('n', '<leader>fu', builtin.buffers, { desc = 'Buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.command_history, {  desc = 'Command History' })
-
 require'colorizer'.setup()
-
-require("which-key").setup({
-  plugins = {
-    marks = false, -- shows a list of your marks on ' and `
-    registers = false, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
-    spelling = {
-      enabled = false, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
-      suggestions = 20, -- how many suggestions should be shown in the list?
-    },
-    presets = {
-      operators = true, -- adds help for operators like d, y, ... and registers them for motion / text object completion
-      motions = false, -- adds help for motions
-      text_objects = true, -- help for text objects triggered after entering an operator
-      windows = true, -- default bindings on <c-w>
-      nav = false, -- misc bindings to work with windows
-      z = false, -- bindings for folds, spelling and others prefixed with z
-      g = true, -- bindings for prefixed with g
-    },
-  },
-  layout = {
-    height = { min = 4, max = 25 }, -- min and max height of the columns
-    width = { min = 20, max = 50 }, -- min and max width of the columns
-    spacing = 3, -- spacing between columns
-    align = "center", -- align columns left, center or right
-  },
-  show_help = true, -- show help message on the command line when the popup is visible
-})
-
 require('lualine').setup({
   options = {
     section_separators = '',
@@ -62,7 +23,6 @@ require('lualine').setup({
     lualine_z = { '' },
   },
 })
-
 require("chatgpt").setup({
       predefined_chat_gpt_prompts = "https://raw.githubusercontent.com/kieran-ohara/dotfiles/main/prompts/prompts.csv",
 })
