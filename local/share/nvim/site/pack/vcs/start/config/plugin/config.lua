d26285881ddfb7a4325f6876785e41fae911605c
Kieran O'Hara <hello@kieranohara.me>
2024-08-04 20:43:41 +0100

    Remove mappings for hunk changes.

--- local/share/nvim/site/pack/vcs/start/config/plugin/config.lua
+++ local/share/nvim/site/pack/vcs/start/config/plugin/config.lua
@@ -20,8 +20,5 @@ require('gitsigns').setup({
       vim.schedule(function() gs.prev_hunk() end)
       return '<Ignore>'
     end, { expr = true })
-
-    map('n', '<leader>hs', gs.stage_buffer)
-    map('n', '<leader>hd', gs.toggle_deleted)
   end
 })
