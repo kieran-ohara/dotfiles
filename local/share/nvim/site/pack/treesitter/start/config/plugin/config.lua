d85eebe9c2c3f0b6c14fa98b841486d8edb3d853
Kieran O'Hara <hello@kieranohara.me>
2024-08-22 13:43:29 +0100

    Format the config.lua file.

--- local/share/nvim/site/pack/treesitter/start/config/plugin/config.lua
+++ local/share/nvim/site/pack/treesitter/start/config/plugin/config.lua
@@ -1,66 +1,54 @@
 -- I install my parsers outside of nvim.
 vim.opt.runtimepath:append(vim.env.XDG_STATE_HOME .. "/parsers")
 
-require 'nvim-treesitter.configs'.setup {
-  ensure_installed = {},
-  incremental_selection = {
-    enable = true;
-  },
-  indent = {
-    enable = true,
-  },
-  highlight = {
-    enable = true,
-  },
-  playground = {
-    enable = true,
-  },
-  textobjects = {
-    select = {
-      enable = true,
-      lookahead = true,
-      keymaps = {
-        ["af"] = "@function.outer",
-        ["if"] = "@function.inner",
-        ["ar"] = "@parameter.outer",
-        ["ir"] = "@parameter.inner",
-      },
-    },
-    swap = {
-      enable = true,
-      swap_next = {
-        ["]s"] = "@parameter.inner",
-      },
-      swap_previous = {
-        ["[s"] = "@parameter.inner",
-      },
-    },
-    move = {
-      enable = true,
-      set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = {
-        ["]m"] = "@function.outer",
-        -- ["]]"] = "@class.outer",
-      },
-      goto_next_end = {
-        -- ["]M"] = "@function.outer",
-        -- ["]["] = "@class.outer",
-      },
-      goto_previous_start = {
-        ["[m"] = "@function.outer",
-        -- ["[["] = "@class.outer",
-      },
-      goto_previous_end = {
-        -- ["[M"] = "@function.outer",
-        -- ["[]"] = "@class.outer",
-      },
-    },
-  },
+require'nvim-treesitter.configs'.setup {
+    ensure_installed = {},
+    incremental_selection = {enable = true},
+    indent = {enable = true},
+    highlight = {enable = true},
+    playground = {enable = true},
+    textobjects = {
+        select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["ar"] = "@parameter.outer",
+                ["ir"] = "@parameter.inner"
+            }
+        },
+        swap = {
+            enable = true,
+            swap_next = {["]s"] = "@parameter.inner"},
+            swap_previous = {["[s"] = "@parameter.inner"}
+        },
+        move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+                ["]m"] = "@function.outer"
+                -- ["]]"] = "@class.outer",
+            },
+            goto_next_end = {
+                -- ["]M"] = "@function.outer",
+                -- ["]["] = "@class.outer",
+            },
+            goto_previous_start = {
+                ["[m"] = "@function.outer"
+                -- ["[["] = "@class.outer",
+            },
+            goto_previous_end = {
+                -- ["[M"] = "@function.outer",
+                -- ["[]"] = "@class.outer",
+            }
+        }
+    }
 }
 
-require("aerial").setup({
-  backends = { "treesitter" },
-  manage_folds = false,
-})
-vim.keymap.set("n", "<leader>ta", "<cmd>AerialToggle<cr>", { desc = "Toggle Aerial" })
-vim.keymap.set("n", "<leader>tp", "<cmd>TSPlaygroundToggle<cr>", { desc = "Toggle Playground" })
+require("aerial").setup({backends = {"treesitter"}, manage_folds = false})
+vim.keymap.set("n", "<leader>ta", "<cmd>AerialToggle<cr>",
+               {desc = "Toggle Aerial"})
+vim.keymap.set("n", "<leader>tp", "<cmd>TSPlaygroundToggle<cr>",
+               {desc = "Toggle Playground"})
+
