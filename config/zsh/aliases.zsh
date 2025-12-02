7dcbd857cdbefa5b1fcbe9ca3c5b334a0d3881bf
Kieran O'Hara <hello@kieranohara.me>
2025-12-02 10:57:53 +0000

    Remove broken aliases.

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -16,14 +16,11 @@ alias dc='docker-compose'
 alias dkp="docker ps -a"
 alias drmoc='docker ps -aqf "status=exited" -f "status=created" | xargs docker rm'
 alias drmoi="docker images -qf \"dangling=true\" | xargs docker rmi"
-alias dv="aws-vault exec kieran -- dvc"
 alias dvm="docker run -it --privileged --pid=host debian nsenter -t 1 -m -u -n -i sh"
 alias e="echo"
 alias fpath="echo $FPATH | sed -r 's/:/\n/g'"
 alias g="git"
 alias h="cd -"
-alias hps=sethttpieprofile
-alias j=opensrcdir
 alias k=kubectl
 alias lgfs="libguestfs"
 alias ll='ls -al'
@@ -31,11 +28,8 @@ alias m=make
 alias md='mkdir -p'
 alias npmi="npm install"
 alias o=open
-alias p="aws-vault exec kieran -- packer"
 alias path="echo $PATH | sed -r 's/:/\n/g'"
 alias rmf='rm -rf'
-alias s=sfdx
-alias ssha="TERM=alacritty ssh"
 alias t="aws-vault exec kieran -- terraform"
 alias tree='() {tree -aC -I ".git|node_modules" --dirsfirst "$@" | less -FRNX;}'
 alias v=resumevimsession
